cc18ffc0f59ec9ccd4c2a6991cab826e973b8054
Öykü Gencay <oyku@Oykus-MacBook-Pro.local>
2015-06-21 14:05:34 +0300

    implement cached solver

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a cache implementation of R function solve.
+## We first prepare a data structure to cache solved result
+## Then provide a caching solve function  
 
-## Write a short comment describing this function
-
+## makeCacheMatrix prepares a data structure to get and set the matrix and cached result
 makeCacheMatrix <- function(x = matrix()) {
-
+    # empty the cache
+    cache <- NULL
+    set <- function(y){
+        x <<- y
+        cache <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv){
+        cache <<- inv
+    }
+    getInverse <- function() cache
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve is a caching solve function. If we already have an inverse, returns it.
+cacheSolve <- function(x, ...) {    
+    # Try to get the cached inverse if exists
+    inv <- x$getInverse()
+    if(!is.null(inv)){
+        # We have a cached copy of the inverse, return it
+        message("getting cached inverse")
+        return inv
+    } else {
+        # We do not have a cached inverse.         
+        # calculate, cache and return
+        matrix <- x$get()        
+        inv <- solve(matrix)
+        x$setInverse(inv)
+        inv
+    }
 }
